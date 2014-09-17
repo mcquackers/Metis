0e697a284e21b1bea2f75c2f1b732a1d6dfb77e2
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-09-17 14:10:32 -0400

    locker complete

---
+++ day3/locker.rb
@@ -0,0 +1,26 @@
+class Locker
+  def initialize
+    @lockers = Array.new(100, "closed")
+  end
+  def gamestart
+    (1..100).each do |student|
+      (1..100).each do |locker|
+        if locker%student == 0
+          toggle(locker-1)
+        end
+      end
+    end
+    @lockers.each_index {|locknum| puts "Locker \##{locknum+1} is #{@lockers[locknum]}"}
+  end
+
+  def toggle(locker)
+    if @lockers[locker] == "closed"
+      @lockers[locker] = "open"
+    else 
+      @lockers[locker]= "closed"
+    end
+  end
+end
+
+locker = Locker.new
+locker.gamestart
